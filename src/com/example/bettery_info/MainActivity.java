dea397b984afc13c50260214d5466c7ed14669c2
Mics <mics.mm@gmail.com>
2013-12-07 08:59:44 +0800

    優化代碼

--- src/com/example/bettery_info/MainActivity.java
+++ src/com/example/bettery_info/MainActivity.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,47 +27,57 @@ public class MainActivity extends Activity {
 	Button resetButton;
 	TextView percentTextView,voltTextView,currentTextView,tempTextView,chargeTextView,USBTextView;
 	
-	String percent = "",volt="",current="",tempTV="",charge="",USB="";
-	double tempTVnum,voltnum,currentnum;
+	// String percent = "",volt="",current="",tempTV="",charge="",USB="";
+	// double tempTVnum,voltnum,currentnum;
 	
+	//Timer設置
+	TimerTask infomation_updater = new TimerTask(){
+		@Override
+		public void run() {
+			runOnUiThread(new Runnable(){
+				@Override
+				public void run() {
+					displayBatteryInfomation(infoListener());    //
+				}
+				
+			});
+		}		
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-			
-		//連結Activity及xml之元件------------------------------------------------------------------------------------
+		viewinit();
+		displayBatteryInfomation(infoListener());    //首次抓取數據 並顯示資訊
+		Timer timer = new Timer();
+		timer.schedule(infomation_updater, 1000,1000);	
+	}
+	
+	
+	
+	private void viewinit(){   //初始化介面
 		percentTextView = (TextView)findViewById(R.id.textView_info_percent);
 		voltTextView = (TextView)findViewById(R.id.textView_info_volt);
 		currentTextView = (TextView)findViewById(R.id.textView_info_current);
 		tempTextView = (TextView)findViewById(R.id.textView_info_temp);
 		chargeTextView = (TextView)findViewById(R.id.textView_info_charge);
 		USBTextView = (TextView)findViewById(R.id.textView_info_USB);
-		//-------------------------------------------------------------------------------------
-		
-		infoListener();    //首次抓取數據
-		
-		
-		//開啟後 一秒更新一次數據-----------------------------------------------------------------------
-		
-		Timer timer = new Timer();
-		TimerTask task = new TimerTask(){
-			@Override
-			public void run() {
-				runOnUiThread(new Runnable(){
-					@Override
-					public void run() {
-						infoListener();
-					}
-					
-				});
-			}		
-		};
-		timer.schedule(task, 1000,1000);	
-		//------------------------------------------------------------------------
+	}
+	
+	
+	private void displayBatteryInfomation(HashMap<String,String> battery_infomation){
+		percentTextView.setText(battery_infomation.get("percent")+"%");
+		voltTextView.setText(battery_infomation.get("voltnum")+"mV");
+		currentTextView.setText(battery_infomation.get("currentnum")+"mA");
+		tempTextView.setText(battery_infomation.get("tempTVnum")+"度");
+		chargeTextView.setText(battery_infomation.get("charge")+"");
+		USBTextView.setText(battery_infomation.get("USB")+"");
 	}
 
-	private void infoListener() {
+	
+	private HashMap<String,String> infoListener() {
+		HashMap<String,String> map = new HashMap<String,String>();
 		String file_name = "/sys/class/power_supply/battery/batt_attr_text";
 		File file =  new File(file_name);
 		if(file.exists())
@@ -76,85 +87,51 @@ public class MainActivity extends Activity {
 			{
 				f = new FileReader(file_name);
 				BufferedReader br = new BufferedReader(f);
-				String temp = "", Allline="";
-				try
+				String temp = "", Allline=""; //what allline do?
+				temp = br.readLine();					
+				while(temp != null)
 				{
-					temp = br.readLine();					
-					while(temp != null)
+					Allline += temp + "\n";
+					temp = br.readLine();
+					if(temp.indexOf("Percentage(%)") >= 0)
 					{
-						Allline += temp + "\n";
-						temp = br.readLine();
-						try
-						{
-							if(temp.indexOf("Percentage(%)") >= 0)
-							{
-								percent = temp.substring(15, temp.length()-1);
-							}
-							if(temp.indexOf("VBAT(uV)") >= 0)
-							{
-								volt = temp.substring(10, temp.length()-1);
-								voltnum = Double.parseDouble(volt);
-								voltnum=voltnum/1000;
-							}
-							if(temp.indexOf("IBAT(uA)") >= 0)
-							{
-								current = temp.substring(10, temp.length()-1);
-								currentnum = Double.parseDouble(current);
-								currentnum = currentnum/1000;
-							}
-							if(temp.indexOf("BATT_TEMP(deci-celsius)") >= 0)
-							{
-								tempTV = temp.substring(25, temp.length()-1);
-								tempTVnum = Double.parseDouble(tempTV);
-								tempTVnum = tempTVnum/10;
-							}
-							if(temp.indexOf("is_charging_enabled") >= 0)
-							{
-								charge = temp.substring(21, temp.length()-1);
-								if("1".equals(charge)){
-									charge = "充電中";
-								}else{
-									charge = "未充電";
-								}
-							}
-							if(temp.indexOf("USBIN_OV_IRQ") >= 0)
-							{
-								USB = temp.substring(14, temp.length()-1);
-								if("0".equals(USB)){
-									USB="USB已連接";
-								}else{
-									USB="未連接USB";
-								}
-							}
-						}
-						catch (Exception e) {
-							
-						}					
+						map.put("percent", temp.substring(15, temp.length()-1));
+					}
+					if(temp.indexOf("VBAT(uV)") >= 0)
+					{
+						map.put("volt", temp.substring(10, temp.length()-1));
+					}
+					if(temp.indexOf("IBAT(uA)") >= 0)
+					{
+						map.put("current", temp.substring(10, temp.length()-1));
+					}
+					if(temp.indexOf("BATT_TEMP(deci-celsius)") >= 0)
+					{
+						map.put("mapTV", temp.substring(25, temp.length()-1));
+					}
+					if(temp.indexOf("is_charging_enabled") >= 0)
+					{
+						map.put("charge", temp.substring(21, temp.length()-1));
+					}
+					if(temp.indexOf("USBIN_OV_IRQ") >= 0)
+					{
+						map.put("USB", temp.substring(14, temp.length()-1));
 					}
 				}
-				catch (IOException e) 
-				{
-					e.printStackTrace();
-				}
-				percentTextView.setText(percent+"%");
-				voltTextView.setText(voltnum+"mV");
-				currentTextView.setText(currentnum+"mA");
-				tempTextView.setText(tempTVnum+"度");
-				chargeTextView.setText(charge+"");
-				USBTextView.setText(USB+"");
+				
 				//toggleGPS();
 				//wifi.setWifiEnabled(true);
 				//mBluetoothAdapter.enable();
-			}
-			catch (FileNotFoundException e)
-			{
+				
+				br.close();
+			}catch (Exception e) {
 				e.printStackTrace();
 			}
+		} else {
+			// percentTextView.setText("File doesn't exist.");
+			// dont find file do
 		}
-		else
-		{
-			percentTextView.setText("File doesn't exist.");
-		}
+		return map;
 		
 	}
 
