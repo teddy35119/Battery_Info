d8de3514ec439ad05f4a3194bdabec2619535675
Mics <mics.mm@gmail.com>
2013-12-18 15:22:50 +0800

    fix display \& add 2 button \& add LogService class

---
+++ src/com/example/bettery_info/LogService.java
@@ -0,0 +1,15 @@
+package com.example.bettery_info;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LogService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
